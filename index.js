9731733e430978b22c5e367029559c8d29072d80
Fikirat Javadov <fikrataj@code.edu.az>
2021-05-03 18:07:59 +0400

    auth

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from "dotenv"
 import postRoutes from './routes/posts.js';
+import userRouter from "./routes/user.js";
 
 
 const app = express();
@@ -15,6 +16,7 @@ app.use(cors());
 
 
 app.use('/posts', postRoutes);
+app.use("/user", userRouter);
 
 app.get("/", (req, res) => {
     res.send("Tech academy")
